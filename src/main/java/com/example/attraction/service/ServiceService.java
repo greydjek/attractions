e07aa8f2fde31e8b3e7b182f7a621ed47bdbaf9a
Artem <delux26rus@mail.ru>
2025-03-18 18:11:23 +0300

    создал таблицы и связи

--- src/main/java/com/example/attraction/service/ServiceService.java
+++ src/main/java/com/example/attraction/service/ServiceService.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 
 @Service
 public class ServiceService {
+    private
     private ServiceRepository serviceRepository;
 
     public ServiceService(ServiceRepository serviceRepository) {
